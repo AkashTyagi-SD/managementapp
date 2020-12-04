b09e3e35728927dc4225681acfaddc9696b24c09
Akash_tyagi_qls <akash.tyagi@quicklivesolutions.com>
2020-12-04 22:45:32 +0530

    Updates

---
+++ precache-manifest.9ea7a7ef56117506b3e66dcfb1684e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4824ae0d37d143af37fc7397a3b56fe2",
+    "url": "/managementapp/index.html"
+  },
+  {
+    "revision": "438e7a0fb4d95b827f33",
+    "url": "/managementapp/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "438e7a0fb4d95b827f33",
+    "url": "/managementapp/static/js/2.6fa12ece.chunk.js"
+  },
+  {
+    "revision": "6553848ede33e774934d8afdf700acf9",
+    "url": "/managementapp/static/js/2.6fa12ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc3b258641592854631",
+    "url": "/managementapp/static/js/main.a48cac1d.chunk.js"
+  },
+  {
+    "revision": "8f71e13c0c9e1b979190",
+    "url": "/managementapp/static/js/runtime-main.942bf4f0.js"
+  }
+]);
